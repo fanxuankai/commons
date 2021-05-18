cff684ccc82b53aeb6cc0aa486460104545191ee
fanxuankai <fan_xuankai@126.com>
2021-05-18 18:55:27 +0800

    数据库树结构实现: Closure Table

--- src/main/java/com/fanxuankai/commons/extra/mybatis/tree/ClosureTable.java
+++ src/main/java/com/fanxuankai/commons/extra/mybatis/tree/ClosureTable.java
@@ -1,139 +1,217 @@
-//package com.fanxuankai.commons.extra.mybatis.tree;
-//
-//import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-//import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-//
-//import java.util.Comparator;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-///**
-// * 闭包表
-// *
-// * @author fanxuankai
-// */
-//public class ClosureTable {
-//    public interface Node extends Entity {
-//        /**
-//         * id
-//         *
-//         * @param id /
-//         */
-//        void setId(Long id);
-//
-//        /**
-//         * 祖先
-//         *
-//         * @return /
-//         */
-//        Long getAncestor();
-//
-//        /**
-//         * 祖先
-//         *
-//         * @param ancestor /
-//         */
-//        void setAncestor(Long ancestor);
-//
-//        /**
-//         * 后代
-//         *
-//         * @return /
-//         */
-//        Long getDescendant();
-//
-//        /**
-//         * 后代
-//         *
-//         * @param descendant /
-//         */
-//        void setDescendant(Long descendant);
-//
-//        /**
-//         * 距离, 与祖先
-//         *
-//         * @return /
-//         */
-//        Integer getDistance();
-//
-//        /**
-//         * 距离
-//         *
-//         * @param distance /
-//         */
-//        void setDistance(Integer distance);
-//    }
-//
-//    public interface Dao<T extends ClosureTable.Node> extends TreeDao<T> {
-//        /**
-//         * 祖先(ancestor)节点：A是所有节点的祖先，F是K与L的祖先。
-//         *
-//         * @param id 节点 id
-//         * @return /
-//         */
-//        @Override
-//        default List<T> ancestors(Long id) {
-//            List<T> list = list(Wrappers.lambdaQuery(entityClass())
-//                    .ne(T::getAncestor, id)
-//                    .eq(T::getDescendant, id));
-//            list.sort(Comparator.comparing(Node::getDistance));
-//            return list;
-//        }
-//
-//        /**
-//         * 子孙(descendant)节点：所有节点是A的子孙，K与L是F的子孙。
-//         *
-//         * @param id 节点 id
-//         * @return /
-//         */
-//        @Override
-//        default List<Descendant<T>> descendants(Long id) {
-//            List<T> list = list(Wrappers.lambdaQuery(entityClass())
-//                    .eq(T::getAncestor, id)
-//                    .ne(T::getDescendant, id));
-//            // todo 构建比较复杂
-//            return children(id)
-//                    .stream()
-//                    .map(o -> new Descendant<>(o, descendants(o.getId())))
-//                    .collect(Collectors.toList());
-//        }
-//
-//        /**
-//         * 父节点(parent node)：B直接连到E与F且只差一个阶度，则B为E与F的父节点
-//         *
-//         * @param id 节点 id
-//         * @return T
-//         */
-//        @Override
-//        default T parent(Long id) {
-//            Long pid = getById(id).getPid();
-//            if (pid == null) {
-//                return null;
-//            }
-//            return getById(pid);
-//        }
-//
-//        /**
-//         * 子节点(children node)：B直接连到E与F且只差一个阶度，则E与F为B的子节点。
-//         *
-//         * @param id 节点 id
-//         * @return /
-//         */
-//        @Override
-//        default List<T> children(Long id) {
-//            return list(Wrappers.lambdaQuery(entityClass())
-//                    .eq(T::getPid, id));
-//        }
-//
-//        /**
-//         * 根节点(root node)：没有父节点的节点，为树的源头。 如：A。
-//         *
-//         * @param wrapper /
-//         * @return /
-//         */
-//        @Override
-//        default List<T> roots(LambdaQueryWrapper<T> wrapper) {
-//            return list(wrapper.isNull(T::getPid));
-//        }
-//    }
-//}
+package com.fanxuankai.commons.extra.mybatis.tree;
+
+import cn.hutool.core.util.ReflectUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.fanxuankai.commons.util.IdUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 闭包表
+ *
+ * @author fanxuankai
+ */
+public class ClosureTable {
+    public interface Node extends Entity {
+        /**
+         * id
+         *
+         * @param id /
+         */
+        void setId(Long id);
+
+        /**
+         * 祖先
+         *
+         * @return /
+         */
+        Long getAncestor();
+
+        /**
+         * 祖先
+         *
+         * @param ancestor /
+         */
+        void setAncestor(Long ancestor);
+
+        /**
+         * 后代
+         *
+         * @return /
+         */
+        Long getDescendant();
+
+        /**
+         * 后代
+         *
+         * @param descendant /
+         */
+        void setDescendant(Long descendant);
+
+        /**
+         * 深度
+         *
+         * @return /
+         */
+        Integer getDepth();
+
+        /**
+         * 深度
+         *
+         * @param depth /
+         */
+        void setDepth(Integer depth);
+    }
+
+    public interface Dao<T extends ClosureTable.Node> extends TreeDao<T> {
+        /**
+         * 祖先(ancestor)节点：A是所有节点的祖先，F是K与L的祖先。
+         *
+         * @param id 节点 id
+         * @return /
+         */
+        @Override
+        default List<T> ancestors(Long id) {
+            List<T> list = list(Wrappers.lambdaQuery(entityClass())
+                    .ne(T::getAncestor, id)
+                    .eq(T::getDescendant, id));
+            list.sort(Comparator.comparing(Node::getDepth));
+            return list;
+        }
+
+        /**
+         * 子孙(descendant)节点：所有节点是A的子孙，K与L是F的子孙。
+         *
+         * @param id 节点 id
+         * @return /
+         */
+        @Override
+        default List<Descendant<T>> descendants(Long id) {
+            List<T> list = list(Wrappers.lambdaQuery(entityClass())
+                    .eq(T::getAncestor, id)
+                    .ne(T::getDescendant, id));
+            return ClosureTableUtils.buildDescendants(id, list);
+        }
+
+        /**
+         * 父节点(parent node)：B直接连到E与F且只差一个阶度，则B为E与F的父节点
+         *
+         * @param id 节点 id
+         * @return T
+         */
+        @Override
+        default T parent(Long id) {
+            return getOne(Wrappers.lambdaQuery(entityClass())
+                    .eq(T::getDescendant, id)
+                    .eq(T::getDepth, 1), false);
+        }
+
+        /**
+         * 子节点(children node)：B直接连到E与F且只差一个阶度，则E与F为B的子节点。
+         *
+         * @param id 节点 id
+         * @return /
+         */
+        @Override
+        default List<T> children(Long id) {
+            return list(Wrappers.lambdaQuery(entityClass())
+                    .eq(T::getAncestor, id)
+                    .eq(T::getDepth, 1));
+        }
+
+        /**
+         * 根节点(root node)：没有父节点的节点，为树的源头。 如：A。
+         *
+         * @param wrapper /
+         * @return /
+         */
+        @Override
+        default List<T> roots(LambdaQueryWrapper<T> wrapper) {
+            return list(wrapper.eq(T::getDepth, 0));
+        }
+
+        /**
+         * 插入新节点
+         *
+         * @param node 节点
+         */
+        @Override
+        default void insertNode(T node) {
+            if (node.getDescendant() == null) {
+                throw new NullPointerException("descendant 不能为空");
+            }
+            if (node.getAncestor() == null
+                    || Objects.equals(node.getAncestor(), node.getDescendant())) {
+                node.setAncestor(node.getDescendant());
+                node.setDepth(0);
+                save(node);
+                return;
+            }
+            List<T> ancestors = ancestors(node.getAncestor());
+            ancestors.forEach(o -> {
+                o.setId(IdUtils.nextId());
+                o.setDescendant(node.getDescendant());
+                o.setDepth(o.getDepth() + 1);
+            });
+            node.setDepth(1);
+            ancestors.add(node);
+            saveBatch(ancestors);
+        }
+
+        /**
+         * 移动节点
+         *
+         * @param id        节点 id
+         * @param targetPid 目标父节点 id
+         */
+        @Override
+        @Transactional(rollbackFor = Exception.class)
+        default void moveNode(Long id, Long targetPid) {
+            // 移除与旧祖先的关联关系
+            remove(Wrappers.lambdaQuery(entityClass())
+                    .eq(T::getDescendant, id));
+
+            // 创建与新祖先的关联关系
+            List<T> ancestors = ancestors(targetPid);
+            ancestors.forEach(o -> {
+                o.setId(IdUtils.nextId());
+                o.setDescendant(id);
+                o.setDepth(o.getDepth() + 1);
+            });
+            T node = ReflectUtil.newInstance(entityClass());
+            node.setId(IdUtils.nextId());
+            node.setAncestor(targetPid);
+            node.setDescendant(id);
+            node.setDepth(1);
+            ancestors.add(node);
+            saveBatch(ancestors);
+
+            // 子节点跟随移动触发更新与祖先的关联关系
+            children(id).forEach(o -> moveNode(o.getDescendant(), o.getAncestor()));
+        }
+
+        /**
+         * 删除节点
+         *
+         * @param id               节点 id
+         * @param removeDescendant 是否删除子孙节点
+         */
+        @Override
+        default void removeNode(Long id, boolean removeDescendant) {
+            // 移除与祖先的关联关系
+            remove(Wrappers.lambdaQuery(entityClass())
+                    .eq(T::getDescendant, id));
+            // 移除与后代的关联关系
+            remove(Wrappers.lambdaQuery(entityClass())
+                    .eq(T::getAncestor, id));
+            if (removeDescendant) {
+                // 删除子节点
+                children(id).forEach(o -> removeNode(o.getDescendant(), true));
+            }
+        }
+    }
+}
--- src/main/java/com/fanxuankai/commons/extra/mybatis/tree/ClosureTableUtils.java
+++ src/main/java/com/fanxuankai/commons/extra/mybatis/tree/ClosureTableUtils.java
@@ -1,55 +1,64 @@
-//package com.fanxuankai.commons.extra.mybatis.tree;
-//
-//import cn.hutool.core.collection.CollectionUtil;
-//
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-///**
-// * @author fanxuankai
-// */
-//public class ClosureTableUtils {
-//    /**
-//     * 构建子孙对象
-//     *
-//     * @param <T>         节点类型
-//     * @param id          节点
-//     * @param descendants 子孙
-//     * @return /
-//     */
-//    public static <T extends ClosureTable.Node> List<Descendant<T>> buildDescendants(Long id, List<T> descendants) {
-//        if (CollectionUtil.isEmpty(descendants)) {
-//            return Collections.emptyList();
-//        }
-//        Map<String, List<T>> groupedByPid =
-//                descendants.stream().collect(Collectors.groupingBy(PathEnumerationsUtils::getParentPath));
-//        return buildDescendants(id, descendants, groupedByPid);
-//    }
-//
-//    /**
-//     * 构建子孙对象
-//     *
-//     * @param pid          父节点 id
-//     * @param descendants  子孙
-//     * @param groupedByPid key: 父节点 value: 子节点列表
-//     * @param <T>          节点类型
-//     * @return /
-//     */
-//    private static <T extends ClosureTable.Node> List<Descendant<T>> buildDescendants(Long pid,
-//                                                                                      List<T> descendants,
-//                                                                                      Map<String, List<T>> groupedByPid) {
-//        if (CollectionUtil.isEmpty(descendants)) {
-//            return Collections.emptyList();
-//        }
-//        List<T> children = groupedByPid.get(pid);
-//        if (CollectionUtil.isEmpty(children)) {
-//            return Collections.emptyList();
-//        }
-//        return children.stream()
-//                .map(t -> new Descendant<T>(t, buildDescendants(t.getPath(), groupedByPid.get(getParentPath(t)),
-//                        groupedByPid)))
-//                .collect(Collectors.toList());
-//    }
-//}
+package com.fanxuankai.commons.extra.mybatis.tree;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.text.StrPool;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author fanxuankai
+ */
+public class ClosureTableUtils {
+    /**
+     * 构建子孙对象
+     *
+     * @param id          节点
+     * @param descendants 子孙
+     * @param <T>         节点类型
+     * @return /
+     */
+    public static <T extends ClosureTable.Node> List<Descendant<T>> buildDescendants(Long id, List<T> descendants) {
+        if (CollectionUtil.isEmpty(descendants)) {
+            return Collections.emptyList();
+        }
+        Map<String, List<T>> groupedByPid =
+                descendants.stream().collect(Collectors.groupingBy(o -> getParentIdentify(o.getAncestor(),
+                        o.getDepth())));
+        return buildDescendants(id, 1, 2, groupedByPid);
+    }
+
+    /**
+     * 构建子孙对象
+     *
+     * @param ancestor     父节点
+     * @param groupedByPid key: 父节点 value: 子节点列表
+     * @param <T>          节点类型
+     * @return /
+     */
+    private static <T extends ClosureTable.Node> List<Descendant<T>> buildDescendants(Long ancestor,
+                                                                                      int depth,
+                                                                                      int level,
+                                                                                      Map<String, List<T>> groupedByPid) {
+        List<T> children = groupedByPid.get(getParentIdentify(ancestor, depth));
+        if (CollectionUtil.isEmpty(children)) {
+            return Collections.emptyList();
+        }
+        return children.stream()
+                .map(o -> new Descendant<>(o, buildDescendants(ancestor, depth + 1, level + 1, groupedByPid), level))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 获取父节点标识
+     *
+     * @param ancestor 祖先
+     * @param depth    深度
+     * @return /
+     */
+    public static String getParentIdentify(Long ancestor, int depth) {
+        return ancestor + StrPool.UNDERLINE + depth;
+    }
+}
